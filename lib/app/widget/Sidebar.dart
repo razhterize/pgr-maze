8c1e1830244ce2e0dbdd5c1bf7ec244ae29eaa3b
razhterize <arza.mahendra.31@gmail.com>
2023-09-09 21:34:22 +0700

    ditch text on sidebar entirely

--- lib/app/widget/Sidebar.dart
+++ lib/app/widget/Sidebar.dart
@@ -14,89 +14,36 @@ class _SidebarState extends State<Sidebar> {
   bool _open = false;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(5.0),
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 350),
-        width: _open ? 100 : 50,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.lightBlueAccent),
-          borderRadius: BorderRadius.circular(4),
-        ),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            MaterialButton(
-              onPressed: () {
-                setState(() {
-                  _open = !_open;
-                });
-              },
-              hoverColor: Colors.blue,
-              child: _open
-                  ? const Row(children: [
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Icon(Icons.menu),
-                      ),
-                      Text("Guilds")
-                    ])
-                  : const Padding(
-                      padding: EdgeInsets.all(4.0),
-                      child: Icon(Icons.menu),
-                    ),
-            ),
-            sidebarItem(Icons.bed_outlined, "Main"),
-            sidebarItem(Icons.alarm, "Kuru"),
-            sidebarItem(Icons.food_bank, "Crepe"),
-          ],
-        ),
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 350),
+      width: _open ? 100 : 50,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.lightBlueAccent),
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: Column(
+        children: [
+          sidebarItem(Icons.bed_outlined, "Main"),
+          sidebarItem(Icons.alarm, "Kuru"),
+          sidebarItem(Icons.food_bank, "Crepe"),
+          Spacer(),
+          sidebarItem(Icons.settings, "Settings")
+        ],
       ),
     );
   }
 
   Widget sidebarItem(IconData icon, String title) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Tooltip(
-          message: title,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25),border: Border.all(color: Colors.black)),
-          // height: 50,
-          // padding: const EdgeInsets.all(8.0),
-          textStyle: const TextStyle(
-            fontSize: 12,
-          ),
-          showDuration: const Duration(seconds: 3),
-          waitDuration: const Duration(milliseconds: 100),
-          child: MaterialButton(
-              onPressed: () {
-                setActiveTable(title);
-              },
-              child: Icon(
-                icon,
-                color: Colors.black,
-              ))),
-    );
-  }
-
-  Widget showTooltip(String title) {
-    return Tooltip(
-      message: title,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(25),
-      ),
-      // height: 50,
-      // padding: const EdgeInsets.all(8.0),
-      preferBelow: true,
-      textStyle: const TextStyle(
-        fontSize: 12,
-      ),
-      showDuration: const Duration(seconds: 3),
-      waitDuration: const Duration(milliseconds: 100),
-      child: const Text('Tap this text and hold down to show a tooltip.'),
+      padding: const EdgeInsets.fromLTRB(2, 8, 2, 8),
+      child: MaterialButton(
+          onPressed: () {
+            setActiveTable(title);
+          },
+          child: Icon(
+            icon,
+            color: Colors.black,
+          )),
     );
   }
 
