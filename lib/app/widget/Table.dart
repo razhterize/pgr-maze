bbb4fd4a5b1c9b4fc9514767d0ed6df0cdb8bf9e
razhterize <arza.mahendra.31@gmail.com>
2023-09-10 00:27:51 +0700

    testing Pocketbase response

--- lib/app/widget/Table.dart
+++ lib/app/widget/Table.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:pocketbase/pocketbase.dart';
+import 'package:scf_maze/app/models/guild.dart';
 
 class MemberTable extends StatefulWidget {
-  const MemberTable({super.key});
+  const MemberTable({super.key, required this.pb, required this.guild});
+
+  final String guild;
+  final PocketBase pb;
 
   @override
   State<MemberTable> createState() => MemberTableState();
 }
 
 class MemberTableState extends State<MemberTable> {
+  Guild? _guild;
+
+  @override
+  void initState() {
+    _guild = Guild(widget.pb, widget.guild);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Table(
-      
+    return Row(
+      children: [
+        MaterialButton(
+          onPressed: () {
+            _guild!.getAll();
+          },
+          child: Icon(Icons.add),
+        ),
+      ],
     );
   }
 }
