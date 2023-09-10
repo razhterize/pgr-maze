5fc49dc4521d1e3158ec1fc965731905d3c8ba7c
razhterize <arza.mahendra.31@gmail.com>
2023-09-10 13:19:59 +0700

    misc function and getter/setter

--- lib/app/models/guild.dart
+++ lib/app/models/guild.dart
@@ -1,22 +1,33 @@
-import 'package:flutter/material.dart';
 import 'package:pocketbase/pocketbase.dart';
 import 'package:scf_maze/app/models/member.dart';
 
 class Guild {
   PocketBase? _pb;
   RecordService? _collection;
+  List<Member> members = [];
+  int totalMembers = 0;
 
   Guild(PocketBase pb, String collection) {
     _pb = pb;
     _collection = _pb?.collection(collection);
+    getAll();
+    return;
   }
 
-  Future<List<RecordModel>?> getAll() async {
-    List<RecordModel>? data = await _collection?.getFullList();
-    List<Member>? members = [];
+  Future<void> getAll({String filter = ""}) async {
+
+    List<RecordModel>? data = await _collection?.getFullList(filter: filter);
     data?.forEach((record) {
       members.add(Member(record));
     });
-    return members;
+    totalMembers = members.length;
   }
+
+  Future<void> syncWithDatabase() async{
+    
+  }
+
+  List<Member> getMembers() => members;
+  void addMember(Member member) => members.add(member);
+  
 }
