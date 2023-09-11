e0932d9d1d303b45c628ec786887692b219b4726
razhterize <arza.mahendra.31@gmail.com>
2023-09-12 00:54:22 +0700

    load .env file on init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app/app.dart';
 
-void main(List<String> args){
+Future<void> main(List<String> args) async {
+  await dotenv.load();
   runApp(const App());
 }
