68c1f4301c786263934c1a093eee446d691a96fc
razhterize <arza.mahendra.31@gmail.com>
2023-09-13 13:11:12 +0700

    debug new instance of Guild class also getter for name cuz debug ffs

--- lib/app/models/guild.dart
+++ lib/app/models/guild.dart
@@ -1,17 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:pocketbase/pocketbase.dart';
 import 'package:scf_maze/app/models/member.dart';
 
 class Guild {
   PocketBase? _pb;
   RecordService? _collection;
+  late String name;
   List<Member> members = [];
   int totalMembers = 0;
 
   Guild(PocketBase pb, String collection) {
     _pb = pb;
     _collection = _pb?.collection(collection);
+    name = collection;
     getAll();
-    return;
+    debugPrint("New Instance of $collection");
   }
 
   Future<void> getAll({String filter = ""}) async {
@@ -24,10 +27,9 @@ class Guild {
   }
 
   Future<void> syncWithDatabase() async{
-    
+    // TODO upload all records to database
   }
 
   List<Member> getMembers() => members;
   void addMember(Member member) => members.add(member);
-  
 }
