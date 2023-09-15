e57818d07561b5392aa62bb7962fb3fa33f410e3
razhterize <arza.mahendra.31@gmail.com>
2023-09-16 03:49:47 +0700

    change window name

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:window_manager/window_manager.dart';
 import 'app/app.dart';
 
 Future<void> main(List<String> args) async {
   await dotenv.load();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await WindowManager.instance.ensureInitialized();
+  windowManager.waitUntilReadyToShow().then((_) async {
+    await windowManager.setTitle('SCF Guild Maze Management');
+  });
   runApp(const App());
 }
