95b942270a6086fe514d9c93b8ae8ae1be0d9dff
razhterize <arza.mahendra.31@gmail.com>
2023-09-16 11:18:59 +0700

    added Android Platform

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:window_manager/window_manager.dart';
 import 'app/app.dart';
 
 Future<void> main(List<String> args) async {
   await dotenv.load();
-  WidgetsFlutterBinding.ensureInitialized();
+  if (!Platform.isAndroid) {
+    WidgetsFlutterBinding.ensureInitialized();
 
-  await WindowManager.instance.ensureInitialized();
-  windowManager.waitUntilReadyToShow().then((_) async {
-    await windowManager.setTitle('SCF Guild Maze Management');
-  });
+    await WindowManager.instance.ensureInitialized();
+    windowManager.waitUntilReadyToShow().then((_) async {
+      await windowManager.setTitle('SCF Guild Maze Management');
+    });
+  }
   runApp(const App());
 }
