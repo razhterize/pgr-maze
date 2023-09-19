d77fae10b97aee6042a782b954feb0bc1c18c42f
razhterize <arza.mahendra.31@gmail.com>
2023-09-19 17:54:23 +0700

    dark/light theme switch

--- lib/app/widgets/sidebar.dart
+++ lib/app/widgets/sidebar.dart
@@ -3,10 +3,14 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 class Sidebar extends StatefulWidget {
   const Sidebar(
-      {super.key, required this.managedGuilds, required this.callbackFunction});
+      {super.key,
+      required this.managedGuilds,
+      required this.callbackFunction,
+      required this.themeChanger});
 
   final List<dynamic> managedGuilds;
   final Function(int) callbackFunction;
+  final Function() themeChanger;
 
   @override
   State<Sidebar> createState() => _SidebarState();
@@ -23,6 +27,7 @@ class _SidebarState extends State<Sidebar> {
 
   late List<dynamic> managedGuilds;
   late Function(int) callbackFunction;
+  bool darkTheme = true;
 
   final Map<String, dynamic> guildIcons = {
     "turu": SvgPicture.asset("assets/slavery.svg", height: 25),
@@ -44,6 +49,7 @@ class _SidebarState extends State<Sidebar> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
+      bottom: false,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Container(
@@ -53,8 +59,30 @@ class _SidebarState extends State<Sidebar> {
           ),
           child: SizedBox(
             width: 50,
-            child: Column(
-              children: _guildSwitcher(),
+            child: ListView.builder(
+              itemCount: managedGuilds.length + 1,
+              itemBuilder: (context, index) {
+                if (index == managedGuilds.length) {
+                  return Container(
+                    decoration: BoxDecoration(
+                        color: darkTheme ? Colors.white : Colors.black,
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            widget.themeChanger();
+                            darkTheme = !darkTheme;
+                          });
+                        },
+                        icon: darkTheme
+                            ? const Icon(Icons.mode_night,color: Colors.black,)
+                            : const Icon(Icons.sunny), color: Colors.white,),
+                  );
+                }
+                String guild = managedGuilds[index];
+                return sidebarItem(guildIcons[guild]!, guild, index);
+              },
             ),
           ),
         ),
@@ -62,24 +90,14 @@ class _SidebarState extends State<Sidebar> {
     );
   }
 
-  List<Widget> _guildSwitcher() {
-    List<Widget> items = [];
-    for (int i = 0; i < managedGuilds.length; i++) {
-      String guild = managedGuilds[i];
-      items.add(sidebarItem(guildIcons[guild]!, guild, i));
-    }
-    return items;
-  }
-
   Widget sidebarItem(dynamic icon, String title, int index) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(2, 8, 2, 8),
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(),
-          borderRadius: BorderRadius.circular(20)
-        ),
+            color: Colors.white,
+            border: Border.all(),
+            borderRadius: BorderRadius.circular(20)),
         child: IconButton(
             onPressed: () {
               callbackFunction(index);
@@ -87,9 +105,5 @@ class _SidebarState extends State<Sidebar> {
             icon: icon),
       ),
     );
-  }
-
-  void setActiveTable(String title) {
-    return;
   }
 }
